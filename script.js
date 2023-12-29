2064261e420be748b87b5012df061a1ef52cf2a3
Elder Gustavo <gustavoelder703@gmail.com>
2023-12-29 19:17:22 -0300

    adjust container width

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function toggleMode() {
 
   const img = document.querySelector("#profile img")
 
-  if(html.classList.contains('light')) {
-    img.setAttribute('src','./assets/avatar-light.png')
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     img.setAttribute("src", "./assets/avatar.png")
   }
@@ -13,6 +13,5 @@ function toggleMode() {
   //if(html.classList.contains('light')){
   //  html.classList.remove('light')
   //} else {
-   // html.classList.add('light') }
-
-  }
+  // html.classList.add('light') }
+}
